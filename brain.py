942ae516eac2ca6078c494f088af9d453b822ecc
Ian <surouxx@gmail.com>
2016-04-07 13:12:13 -0400

    Here lies Sigma-Omega's Brain

--- brain.py
+++ brain.py
@@ -1,108 +1,95 @@
 // Project Name: Sigma-Omega
 
-
-#pragma warning(disable: 4786)
-
-include <iostream>
-include <string>
-include <vector>
-include <ctime>
-
-const int MAX_RESP = 3;
-
-typedef std::vector<std::string> vstring;
-
-vstring find_match(std::string input);
-void copy(char *array[], vstring &v);
-
-
-typedef struct {
-    char *input;
-    char *responses[MAX_RESP];
-}record;
-
-record KnowledgeBase[] = {
-    {"WHAT IS YOUR NAME", 
-    {"MY NAME IS CHATTERBOT2.",
-     "YOU CAN CALL ME CHATTERBOT2.",
-     "WHY DO YOU WANT TO KNOW MY NAME?"}
-    },
-
-    {"HI", 
-    {"HI THERE!",
-     "HOW ARE YOU?",
-     "HI!"}
-    },
-    
-    {"HOW ARE YOU",
-    {"I'M DOING FINE!",
-    "I'M DOING WELL AND YOU?",
-    "WHY DO YOU WANT TO KNOW HOW AM I DOING?"}
-    },
-
-    {"WHO ARE YOU",
-    {"I'M AN A.I PROGRAM.",
-     "I THINK THAT YOU KNOW WHO I'M.",
-     "WHY ARE YOU ASKING?"}
-    },
-
-    {"ARE YOU INTELLIGENT",
-    {"YES,OFCORSE.",
-     "WHAT DO YOU THINK?",
-     "ACTUALY,I'M VERY INTELLIGENT!"}
-    },
-
-    {"ARE YOU REAL",
-    {"DOES THAT QUESTION REALLY MATERS TO YOU?",
-     "WHAT DO YOU MEAN BY THAT?",
-     "I'M AS REAL AS I CAN BE."}
-    }
-};
-
-size_t nKnowledgeBaseSize = sizeof(KnowledgeBase)/sizeof(KnowledgeBase[0]);
-
-
-int main() {
-    srand((unsigned) time(NULL));
-
-    std::string sInput = "";
-    std::string sResponse = "";
-
-    while(1) {
-        std::cout << ">";
-        std::getline(std::cin, sInput);
-        vstring responses = find_match(sInput);
-        if(sInput == "BYE") {
-            std::cout << "IT WAS NICE TALKING TO YOU USER, SEE YOU NEXTTIME!" << std::endl;  
-            break;
-        } 
-        else if(responses.size() == 0)  {
-            std::cout << "I'M NOT SURE IF I  UNDERSTAND WHAT YOU  ARE TALKING ABOUT." << std::endl;
-        }
-        else {
-            int nSelection = rand()  % MAX_RESP;
-            sResponse =   responses[nSelection]; std::cout << sResponse << std::endl; 
-        } 
-    } 
-
-    return 0;
-}
-    
-// make a  search for the  user's input 
-// inside the database of the program 
-vstring find_match(std::string  input) { 
-    vstring result;
-    for(int i = 0; i < nKnowledgeBaseSize;  ++i) {  
-        if(std::string(KnowledgeBase[i].input) == input) { 
-            copy(KnowledgeBase[i].responses, result); 
-            return result;
-        } 
-    } 
-    return result; 
-}
-
-void copy(char  *array[], vstring &v) { 
-    for(int i = 0;  i < MAX_RESP; ++i) {
-        v.push_back(array[i]);
-    }
-}
+import re
+import sqlite3
+from collections import Counter
+from string import punctuation
+from math import sqrt
+ 
+# initialize the connection to the database
+connection = sqlite3.connect('chatbot.sqlite')
+cursor = connection.cursor()
+ 
+# create the tables needed by the program
+try:
+    # create the table containing the words
+    cursor.execute('''
+        CREATE TABLE words (
+            word TEXT UNIQUE
+        )
+    ''')
+    # create the table containing the sentences
+    cursor.execute('''
+        CREATE TABLE sentences (
+            sentence TEXT UNIQUE,
+            used INT NOT NULL DEFAULT 0
+        )''')
+    # create association between weighted words and the next sentence
+    cursor.execute('''
+        CREATE TABLE associations (
+            word_id INT NOT NULL,
+            sentence_id INT NOT NULL,
+            weight REAL NOT NULL)
+    ''')
+except:
+    pass
+ 
+def get_id(entityName, text):
+    """Retrieve an entity's unique ID from the database, given its associated text.
+    If the row is not already present, it is inserted.
+    The entity can either be a sentence or a word."""
+    tableName = entityName + 's'
+    columnName = entityName
+    cursor.execute('SELECT rowid FROM ' + tableName + ' WHERE ' + columnName + ' = ?', (text,))
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+    else:
+        cursor.execute('INSERT INTO ' + tableName + ' (' + columnName + ') VALUES (?)', (text,))
+        return cursor.lastrowid
+ 
+def get_words(text):
+    """Retrieve the words present in a given string of text.
+    The return value is a list of tuples where the first member is a lowercase word,
+    and the second member the number of time it is present in the text."""
+    wordsRegexpString = '(?:\w+|[' + re.escape(punctuation) + ']+)'
+    wordsRegexp = re.compile(wordsRegexpString)
+    wordsList = wordsRegexp.findall(text.lower())
+    return Counter(wordsList).items()
+ 
+ 
+B = 'Hello!'
+while True:
+    # output bot's message
+    print('B: ' + B)
+    # ask for user input; if blank line, exit the loop
+    H = raw_input('H: ').strip()
+    if H == '':
+        break
+    # store the association between the bot's message words and the user's response
+    words = get_words(B)
+    words_length = sum([n * len(word) for word, n in words])
+    sentence_id = get_id('sentence', H)
+    for word, n in words:
+        word_id = get_id('word', word)
+        weight = sqrt(n / float(words_length))
+        cursor.execute('INSERT INTO associations VALUES (?, ?, ?)', (word_id, sentence_id, weight))
+    connection.commit()
+    # retrieve the most likely answer from the database
+    cursor.execute('CREATE TEMPORARY TABLE results(sentence_id INT, sentence TEXT, weight REAL)')
+    words = get_words(H)
+    words_length = sum([n * len(word) for word, n in words])
+    for word, n in words:
+        weight = sqrt(n / float(words_length))
+        cursor.execute('INSERT INTO results SELECT associations.sentence_id, sentences.sentence, ?*associations.weight/(4+sentences.used) FROM words INNER JOIN associations ON associations.word_id=words.rowid INNER JOIN sentences ON sentences.rowid=associations.sentence_id WHERE words.word=?', (weight, word,))
+    # if matches were found, give the best one
+    cursor.execute('SELECT sentence_id, sentence, SUM(weight) AS sum_weight FROM results GROUP BY sentence_id ORDER BY sum_weight DESC LIMIT 1')
+    row = cursor.fetchone()
+    cursor.execute('DROP TABLE results')
+    # otherwise, just randomly pick one of the least used sentences
+    if row is None:
+        cursor.execute('SELECT rowid, sentence FROM sentences WHERE used = (SELECT MIN(used) FROM sentences) ORDER BY RANDOM() LIMIT 1')
+        row = cursor.fetchone()
+    # tell the database the sentence has been used once more, and prepare the sentence
+    B = row[1]
+    cursor.execute('UPDATE sentences SET used=used+1 WHERE rowid=?', (row[0],))
